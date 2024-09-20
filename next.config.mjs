97f4b450b80129c12e97de62697ee2226549238b
Szymczek <szymon.szymczewski@protonmail.com>
2024-09-20 16:36:18 +0200

    feat: add config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    MY_VARIABLE: process.env.MY_VARIABLE,
+    SITE_URL:process.env.SITE_URL,
+    USOS_CONSUMER_KEY: process.env.USOS_CONSUMER_KEY,
+    USOS_CONSUMER_SECRET: process.env.USOS_CONSUMER_SECRET,
+    USOS_BASE_URL: process.env.USOS_BASE_URL
+  },
   images: {
     remotePatterns: [
       {
